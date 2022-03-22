222c56d9c15188720423d1da5cc3afea6b7083ba
박주미 <zummy@kakao.com>
2022-03-22 22:47:37 +0900

    Feat : Coin Tracker 만들기

--- ReactJS로 영화 웹 서비스 만들기/react-for-beginners/src/App.js
+++ ReactJS로 영화 웹 서비스 만들기/react-for-beginners/src/App.js
@@ -1,30 +1,29 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  // 한 번만 실행
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, [])
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder='Write your to do ...' />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select>
+        {coins.map((coin) =>
+          <option>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>)}
+      </select>
     </div>
   );
 }
